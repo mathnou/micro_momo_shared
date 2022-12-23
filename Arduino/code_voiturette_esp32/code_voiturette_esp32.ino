c60d5f03bfd587f1948a684766e2f485de0fbd0f
mathnou <mathilde.nousse.work@gmail.com>
2022-12-23 18:16:23 +0100

    Send batteryLevel, lum + add manual and auto modes
    Yo man, j'ai ajouté l'envoi des niveaux de batterie et de luminosité. A chaque fois j'explique le format envoyé en commentaires.
    J'ai essayé aussi le switch entre modes manuels et auto pour les feux, ça me parait bien mais c'est pas testé. Donc à voir avec l'app après. Pareil en commentaires il y a le format attendu pour la gestion des paramètres.
    
    La bise et bonne soirée

--- Arduino/code_voiturette_esp32/code_voiturette_esp32.ino
+++ Arduino/code_voiturette_esp32/code_voiturette_esp32.ino
@@ -13,11 +13,17 @@ BluetoothSerial ESP_BT;
 #define pinLEDBlanche 26
 
 #define LDR 27 // Photoresistance 
+boolean isFeuxAuto = true; //Boolean qui determine si on est en mode auto ou non pour les feux
+
+#define pinBatteryLevel 4 //Pont diviseur de tension pour niveau de batterie
+float coeffPont;
+
 
 float pourcentageVitesse=0;
 
 boolean isAvance = false;
 boolean isRecule = false;
+
 
 
 
@@ -34,6 +40,9 @@ void setup() {
   pinMode(pinLEDBlanche, OUTPUT);
 
   pinMode(LDR, INPUT);
+
+  pinMode(pinBatteryLevel, INPUT);
+  coeffPont = (float)200/(200+51); //Valeurs de resistances utilisées
 }
 
 void loop() {
@@ -44,7 +53,11 @@ void loop() {
     Serial.println("Bluetooth connected");
     while(ESP_BT.hasClient()) 
     { 
-      checkStateLights();
+      if(isFeuxAuto){
+        checkStateLights();
+      }
+      sendLightLevel();
+      sendBatteryLevel();
       int received;
       received = ESP_BT.read();        
 
@@ -85,13 +98,37 @@ void loop() {
           stopCar();
 
       }    
+
+      else if((char)received=='p'){ //P comme param. Le format d'un parametre est "p-FA-O-w" avec FA pour Feux Autos et O pour ON par ex (on utilisera F pour OFF)
+          Serial.println("Paramètres");
+          String chaine="";
+          while((char)received!='w'){
+            Serial.println((char)received);
+            chaine += (char)received;
+            received=ESP_BT.read();
+          }
+          if(chaine.substring(1,3)=="FA"){ //Pour Feux Autos
+            isFeuxAuto = (chaine.substring(4,5)=="0");
+          }
+          else if(chaine.substring(1,3)=="FM"){ //Pour feux manuels. Envoi de "p-FM-O" pour allumer les feux en mode manuel, envoi de "p-FM-F" pour les éteidre
+              if(chaine.substring(4,5)=="O"){
+                digitalWrite(pinLEDRouge, HIGH);
+                digitalWrite(pinLEDBlanche, HIGH);
+              }
+              else{
+                digitalWrite(pinLEDRouge, LOW);
+                digitalWrite(pinLEDBlanche, LOW);
+              }
+          }                    
+
+      }   
     }
   }
   else{
     Serial.println("Bluetooth NOT connected");
     stopCar();
   }
-  checkStateLights();
+
   
 }
 
@@ -127,7 +164,18 @@ void roue_droite(float pourcentageVitesse){
   }
 }
 
-void checkStateLights(){
+
+void stopCar(){
+  analogWrite( pinINA1, LOW);
+  analogWrite(pinINB1, LOW);
+  analogWrite( pinINA2, LOW);
+  analogWrite(pinINB2, LOW);
+  Serial.println("STOP");
+  isAvance=false;
+  isRecule=false;
+}
+
+void checkStateLights(){ 
   int value = analogRead(LDR);
   if(value>2000){
     digitalWrite(pinLEDRouge, HIGH);
@@ -139,12 +187,27 @@ void checkStateLights(){
   }
 }
 
-void stopCar(){
-  analogWrite( pinINA1, LOW);
-  analogWrite(pinINB1, LOW);
-  analogWrite( pinINA2, LOW);
-  analogWrite(pinINB2, LOW);
-  Serial.println("STOP");
-  isAvance=false;
-  isRecule=false;
+void sendLightLevel(){ //Le format d'envoi est "L56w" pour un niveau à 56% par ex
+  int value = analogRead(LDR);
+  String toSendLum = "L" + String(value*100/4096);
+  toSendLum += "w"; //Pour signifier la fin du message
+  ESP_BT.println(toSendLum);
 }
+
+
+void sendBatteryLevel(){ //Le format d'envoi est "B57w" pour un niveau à 57% par ex
+  int levelBatDigit = analogRead(pinBatteryLevel);
+  float tensionBat = (float)levelBatDigit*(3.3 / (float)4095) / (float)coeffPont;  //Calcul fait pour une batterie lithium
+  float coeffDir = (float)100/(4.2-1.9);
+  float ordonnee = (float)100-coeffDir*4.2;
+  float pourcentageLevel = tensionBat*coeffDir + ordonnee ;
+  Serial.println("pourcentage");
+  Serial.println(pourcentageLevel);
+  String toSendBattery = "B" + String(pourcentageLevel);
+  toSendBattery += "w"; //Pour signifier la fin du message
+  ESP_BT.println(toSendBattery);
+}
+
+
+
+
